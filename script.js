5b84a4ba1c4b8ad03be037a617b4dc9643784575
Mahi-Shrma <151066048+Mahi-Shrma@users.noreply.github.com>
2024-11-09 00:44:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.getElementById("form").addEventListener('submit',function(e){
+	e.preventDefault();
+		const age=parseInt(document.getElementById("age").value);
+		const name=document.getElementById("name").value;
+	
+	return new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+		        if(age>18){
+			    resolve(`Welcome, ${name}. You can vote.`);
+				}
+				else{
+			reject(`Oh sorry ${name}. You aren't old enough.`);
+		}
+	},4000);
+}).then((msg)=>alert(msg))
+		.catch((er)=>alert(er));
+});
